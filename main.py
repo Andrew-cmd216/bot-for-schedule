7eb62abbbadf35a5fa3948abcdba2ab8b565567e
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2025-10-14 20:34:56 +0300

    kinda success

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+from pathlib import Path
+
+
+PROJECT_PATH = Path(__file__).parent
+CREDENTIALS_PATH = PROJECT_PATH / 'Credential' / 'Credentials.json'
+
 
 # Define the scope
 scope = [
@@ -8,11 +14,18 @@ scope = [
     'https://www.googleapis.com/auth/drive'
 ]
 
+
 # Add credentials
-creds = Credentials.from_service_account_file('Credential/Credentials.json', scopes=scope)
+creds = Credentials.from_service_account_file(str(CREDENTIALS_PATH), scopes=scope)
+
 
 # Authenticate and create the client
 client = gspread.authorize(creds)
 
+
 # Open the spreadsheet
-sheet = client.open('Your Sheet Name').sheet1
+sheet = client.open('кадабра1').sheet1
+
+data = sheet.get_values()
+df = pd.DataFrame(data)
+print(df)
